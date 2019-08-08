5abc6748c92a48d68c0b6f73e8d50ae095861cb0
navrajkambo <navrajkambo@users.noreply.github.com>
2019-08-08 15:08:16 -0700

    - PLL & SDRAM added
    added PLL and SDRAM, but didn't connect them between codec

--- AudioFX.sv
+++ AudioFX.sv
@@ -19,15 +19,30 @@ module AudioFX(
 	AUD_ADCLRCK,
 	AUD_DACLRCK,
 	FPGA_I2C_SDAT,
+	DRAM_DQ,
 
 	// Outputs
 	AUD_XCK,
 	AUD_DACDAT,
 	LEDR,
 	FPGA_I2C_SCLK,
-	GPIO_0
+	GPIO_0,
+	DRAM_ADDR,
+	DRAM_BA,
+	DRAM_CAS_N, 
+	DRAM_RAS_N, 
+	DRAM_CLK,
+	DRAM_CKE, 
+	DRAM_CS_N, 
+	DRAM_WE_N,
+	DRAM_UDQM,
+	DRAM_LDQM
 	);
+	
+	// params
 	parameter DATA_W = 16;
+	parameter ADDR_W = 25;
+	
 	// signals				
 	input [9:0] SW;
 	input CLOCK_50;
@@ -38,6 +53,7 @@ module AudioFX(
 	inout	AUD_ADCLRCK;
 	inout	AUD_DACLRCK;
 	inout FPGA_I2C_SDAT;
+	inout [15:0] DRAM_DQ;
 
 	output AUD_XCK;
 	output AUD_DACDAT;
@@ -45,11 +61,18 @@ module AudioFX(
 	output [9:0] LEDR;
 	output [DATA_W-1:0] GPIO_0;
 	
+	output [12:0] DRAM_ADDR;
+	output [1:0] DRAM_BA;
+	output DRAM_CAS_N, DRAM_RAS_N, DRAM_CLK;
+	output DRAM_CKE, DRAM_CS_N, DRAM_WE_N;
+	output DRAM_UDQM;
+	output DRAM_LDQM;
+	
 	// registers
 	reg [22:0] count;
 	
-	// logic & wires
-	logic reset_out;
+	// logic & wires for ADC/DAC
+	logic [1:0] reset_out;
 	logic [1:0][DATA_W-1:0] DAC_Data, ADC_Data;
 	logic [1:0] DAC_Ready, ADC_Ready, DAC_Valid, ADC_Valid;
 	
@@ -59,6 +82,17 @@ module AudioFX(
 	logic [3:0] i2c_byte_enable = 4'b1111;
 	logic i2c_read=0, i2c_write = 0, i2c_waitrequest;
 	
+	// logic for RAM
+	logic [1:0][ADDR_W-1:0] waddr, raddr;
+	logic [1:0][DATA_W-1:0] wdata;
+	logic [ADDR_W-1:0] waddress, raddress;
+	logic [DATA_W-1:0] writedata, readdata;
+	logic [1:0] write, read;
+	logic read_ready, busy, we, re;
+	logic CLOCK_50_D;
+ 	
+	
+	
 	// instantiations
 	
 	// Audio PLL
@@ -66,10 +100,18 @@ module AudioFX(
 		.ref_clk_clk        (CLOCK_50),        //      ref_clk.clk
 		.ref_reset_reset    (~KEY[0]),    //    ref_reset.reset
 		.audio_clk_clk      (AUD_XCK),      //    audio_clk.clk
-		.reset_source_reset (reset_out)  // reset_source.reset
+		.reset_source_reset (reset_out[0])  // reset_source.reset
+	);
+	// RAM PLL
+	SDRAMPLL u1 (
+		.ref_clk_clk        (CLOCK_50),        //      ref_clk.clk
+		.ref_reset_reset    (~KEY[0]),    //    ref_reset.reset
+		.sys_clk_clk        (CLOCK_50_D),        //      sys_clk.clk
+		.sdram_clk_clk      (DRAM_CLK),      //    sdram_clk.clk
+		.reset_source_reset (reset_out[1])  // reset_source.reset
 	);
 	// Audio Config (16bit audio, you can change this with QSYS)
-	AVConfig u1 (
+	AVConfig u2 (
 		.clk         (CLOCK_50),         //                    clk.clk
 		.reset       (~KEY[0]),       //                  reset.reset
 		.address     (i2c_read_data),     // avalon_av_config_slave.address
@@ -83,7 +125,7 @@ module AudioFX(
 		.I2C_SCLK    (FPGA_I2C_SCLK)     //                       .SCLK
 	);
 	// Audio Codec
-	AudioCodec u2 (
+	AudioCodec u3 (
 		.clk                          (CLOCK_50),                          //                         clk.clk
 		.reset                        (~KEY[0]),                        //                       reset.reset
 		.AUD_ADCDAT                   (AUD_ADCDAT),                   //          external_interface.ADCDAT
@@ -104,7 +146,34 @@ module AudioFX(
 		.to_dac_right_channel_valid   (DAC_Valid[1]),   //                            .valid
 		.to_dac_right_channel_ready   (DAC_Ready[1])    //                            .ready
 	);
-	
+	// SDRAM hardware-only controller
+	sdram_controller u4(
+		/* HOST INTERFACE */
+		.wr_addr(waddress),
+		.wr_data(writedata),
+		.wr_enable(we),
+
+		.rd_addr(raddress),
+		.rd_data(readdata),
+		.rd_ready(read_ready),
+		.rd_enable(re),
+
+		.busy(busy), 
+		.rst_n(KEY[0]), 
+		.clk(CLOCK_50),
+
+		/* SDRAM SIDE */
+		.addr(DRAM_ADDR), 
+		.bank_addr(DRAM_BA), 
+		.data(DRAM_DQ), 
+		.clock_enable(DRAM_CKE), 
+		.cs_n(DRAM_CS_N), 
+		.ras_n(DRAM_RAS_N), 
+		.cas_n(DRAM_CAS_N), 
+		.we_n(DRAM_WE_N),
+		.data_mask_low(DRAM_LDQM), 
+		.data_mask_high(DRAM_UDQM)
+	);
 	// Useful for signal tap or scope debugging and
 	assign GPIO_0[DATA_W-1:0] = DAC_Data[0];
 	
@@ -119,6 +188,14 @@ module AudioFX(
 			LEDR[0] <= 0;
 			count <= 1;
 		end
+	end
+	
+	// Logic for not using SDRAM
+	always@(posedge CLOCK_50) begin
+		we <= 0;
+		re <= 0;
+		waddress = '0;
+		raddress = '0;
 	end
 	
 	// Basic tie-back for audio ADC -> DAC
--- db/ip/sld30451bca/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld30451bca/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 9679eec2749e0bcd013d
+//   DESIGN_HASH   : abcba009f7d39491d741
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9679eec2749e0bcd013d };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'habcba009f7d39491d741 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
